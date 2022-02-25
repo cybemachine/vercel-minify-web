451c25450701b4e412a9bb954afba8c293615c56
cybemachine <80441770+cybemachine@users.noreply.github.com>
2022-02-25 16:50:44 +0000

    added swagger

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const UglifyJS = require("uglify-js");
+const swaggerUi = require('swagger-ui-express');
 const exp = require("express");
 const path = require("path");
 const app = exp();
+const swaggerDocument = require('./swagger.json');
+
+app.use(exp.static(__dirname + '/docs/'));
 
 app.all('/', (req, res) => {
-    res.sendFile(path.resolve("./index.html"))
-})
+    res.redirect(302, 'api-docs')
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.disable('x-powered-by')
 
 app.route('/api')
     .get((req, res) => {
@@ -13,8 +21,18 @@ app.route('/api')
         res.send(UglifyJS.minify(req.query.code || req.query.input))
     })
     .post((req, res) => {
-        if (!req.body) return res.send("no body detected");
-        res.send(UglifyJS.minify(req.body))
+        let code = ''
+        if (req.query.code || req.query.input) {
+            code = UglifyJS.minify(req.query.code || req.query.input);
+        } else {
+            if (!req.body) {
+                res.send("no body detected")
+            } else {
+                code = UglifyJS.minify(req.body)
+            }
+        }
+
+        res.send(code)
     });
 
 app.listen(8080)
